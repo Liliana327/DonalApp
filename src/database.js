0145e7af5e08cf489acc4f8af09fb9c7976bed1e
LILIANA <liliana@MacBook-Pro-de-LILIANA.local>
2020-03-24 13:59:01 -0500

    add

--- src/database.js
+++ src/database.js
@@ -1,24 +1,10 @@
 const mongoose = require('mongoose');
-var options = {
-  server: {
-    socketOptions: {
-      keepAlive: 300000,
-      connectTimeoutMS: 30000,
-    }
-  },
-  replset: {
-    socketOptions: {
-      keepAlive: 300000,
-      connectTimeoutMS: 30000
-    }
-  }
-}
 
-mongoose.connect('mongodb+srv://liliana:Test1234@cluster0-3kiks.mongodb.net/test?retryWrites=true&w=majority', {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+mongoose.connect('mongodb://localhost/DonalApp-db-app', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
 })
 .then(db => console.log('DB is conneted'))
 .catch(err => console.error('err'));
