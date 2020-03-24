0aff7d6739e1bbab903441d8557cd5f7da1365a7
LILIANA <liliana@MacBook-Pro-de-LILIANA.local>
2020-03-23 21:18:12 -0500

    modifaile

--- src/database.js
+++ src/database.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
+var options = {
+  server: {
+    socketOptions: {
+      keepAlive: 300000,
+      connectTimeoutMS: 30000,
+    }
+  },
+  replset: {
+    socketOptions: {
+      keepAlive: 300000,
+      connectTimeoutMS: 30000
+    }
+  }
+}
 
 mongoose.connect('mongodb+srv://liliana:Test1234@cluster0-3kiks.mongodb.net/test?retryWrites=true&w=majority', {
-    useCreateIndex: true,
+  ...options
+/*     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true */
 })
 .then(db => console.log('DB is conneted'))
 .catch(err => console.error('err'));
